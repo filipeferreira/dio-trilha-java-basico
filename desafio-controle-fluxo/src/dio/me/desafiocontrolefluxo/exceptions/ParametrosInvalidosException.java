68fee00e90729be8a8e98b6688cc702fa5840eea
Filipe Ferreira <filipef@ciasc.sc.gov.br>
2025-02-21 07:16:17 -0300

    Desafio controle de fluxo trilha java básico

---
+++ desafio-controle-fluxo/src/dio/me/desafiocontrolefluxo/exceptions/ParametrosInvalidosException.java
@@ -0,0 +1,7 @@
+package dio.me.desafiocontrolefluxo.exceptions;
+
+public class ParametrosInvalidosException extends Exception {
+    public ParametrosInvalidosException(String message) {
+        super(message);
+    }
+}
